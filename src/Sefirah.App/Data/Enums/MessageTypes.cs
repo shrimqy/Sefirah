200172e0599f729240ea5da1f17d675f95fd4459
shrimqy <shrimqy@gmail.com>
2025-01-23 13:41:05 +0530

    Remove unused enum types

--- src/Sefirah.App/Data/Enums/MessageTypes.cs
+++ src/Sefirah.App/Data/Enums/MessageTypes.cs
@@ -1,53 +1,6 @@
 ﻿using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
 
 namespace Sefirah.App.Data.Enums;
-public enum MessageType
-{
-    [EnumMember(Value = "0")]
-    Misc,
-    [EnumMember(Value = "1")]
-    DeviceInfo,
-    [EnumMember(Value = "2")]
-    DeviceStatus,
-    [EnumMember(Value = "3")]
-    Clipboard,
-    [EnumMember(Value = "4")]
-    Notification,
-    [EnumMember(Value = "5")]
-    NotificationAction,
-    [EnumMember(Value = "6")]
-    ReplyAction,
-    [EnumMember(Value = "7")]
-    PlaybackData,
-    [EnumMember(Value = "8")]
-    FileTransferInfo,
-    [EnumMember(Value = "9")]
-    InteractiveControl,
-    [EnumMember(Value = "10")]
-    ApplicationInfo,
-    [EnumMember(Value = "11")]
-    SftpServerInfo,
-    [EnumMember(Value = "12")]
-    UdpBroadcast
-}
-
-public enum InteractiveControlType
-{
-    [EnumMember(Value = "SINGLE")]
-    SingleTapEvent,
-    [EnumMember(Value = "HOLD")]
-    HoldTapEvent,
-    [EnumMember(Value = "SWIPE")]
-    SwipeEvent,
-    [EnumMember(Value = "KEYBOARD")]
-    KeyboardEvent,
-    [EnumMember(Value = "SCROLL")]
-    ScrollEvent,
-    [EnumMember(Value = "KEY")]
-    KeyEvent,
-}
-
 public enum FileTransferType
 {
     [EnumMember(Value = "BulkFile")]
@@ -84,21 +37,4 @@ public enum NotificationType
     Removed,
     New,
     Action
-}
-
-public enum ClipboardType
-{
-    Text,
-    Image
-}
-
-public enum ScrollDirection
-{
-    Up,
-    Down
-}
-
-public enum KeyboardActionType
-{
-    Tab, Backspace, Enter, Escape, CtrlC, CtrlV, CtrlX, CtrlA, CtrlZ, CtrlY, Shift
 }
