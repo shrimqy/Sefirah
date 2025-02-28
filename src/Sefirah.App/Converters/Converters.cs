a7f0c42d2209f82f8e704e302b89c519eae572e4
shrimqy <shrimqy@gmail.com>
2025-02-28 17:50:08 +0530

    remove title if there is no paired devices

--- src/Sefirah.App/Converters/Converters.cs
+++ src/Sefirah.App/Converters/Converters.cs
@@ -324,6 +324,23 @@ public class NullToVisibilityConverter : IValueConverter
     }
 }
 
+public class CountToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is int count && count == 0)
+        {
+            return Visibility.Collapsed;
+        }
+        return Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
+
 public class InverseNullToVisibilityConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, string language)
